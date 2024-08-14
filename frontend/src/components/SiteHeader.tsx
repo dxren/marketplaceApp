53cf777a9354706425560ad160a8a41e922a108a
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-08-14 10:46:46 -0400

    header and userinfo page and style updates

--- frontend/src/components/SiteHeader.tsx
+++ frontend/src/components/SiteHeader.tsx
@@ -1,17 +1,38 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { LucideSearch } from "lucide-react";
 
 function SiteHeader() {
     return (
         <>
-        <header className="grenze-gotisch-title">
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px' }}>
-            <div style={{ fontSize: '60px', margin: 0 }}>fractal marketplace</div>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
+        <header>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '10px', marginLeft: '4px', marginRight: '10px' }}>
+            <div className="title" >fractal marketplace</div>
+            <div style={{display: 'flex', gap: '80px', fontSize: '24px', fontFamily: 'Brygada 1918', marginTop: '10px', marginLeft: '20px'}}>
+                <div> feed </div>
+                <div> profile </div>
+                <div> about </div>
+            </div>
+            <div style={{display: 'flex'}}> 
+                <LucideSearch style={{marginRight: '8px', marginTop: '3px',}}/>
+                <input onBlur={() => console.log('search blur')} onFocus={() => console.log('search focus')} type="text" placeholder="Search" style={{
+                    fontSize: '16px', 
+                    fontWeight: 'bold',
+                    marginRight: '30px',
+                    paddingLeft: '8px',
+                    background: 'transparent',
+                    border: '2px solid #fff9e6',
+                    color: '#fff9e6',
+                    borderRadius: '4px',
+                    width: '100px'
+                }} /> 
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+            </div>
+
         </div>
     </header>
     </>
