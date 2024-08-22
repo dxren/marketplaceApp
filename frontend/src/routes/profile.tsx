202086ae192623b886107966e55d232b8d8c8a81
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-08-22 18:19:08 -0400

    sfds

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
 import UserInfo from "../components/UserInfo/UserInfo";
 
 export default function ProfilePage() {
     return (
         <>
             <UserInfo userId={null} />
-    
         </>
     )
 }
