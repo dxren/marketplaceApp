896f337256d5093cd74d8f9deb241d9705ae01cb
dxren <dorothy.x.ren@gmail.com>
2024-08-23 15:02:21 -0400

    more styling tweaks

--- frontend/src/components/UserInfo/FavoriteAsksFeed.tsx
+++ frontend/src/components/UserInfo/FavoriteAsksFeed.tsx
@@ -26,20 +26,39 @@ function FavoriteAsksFeed() {
 
     return (
         <>
-            <div style={{ flex: 1 }}>
-                <div style={{ display: 'flex', gap: '10px', fontSize: '1.75rem', fontWeight: '550', marginBottom: '10px' }}>
-                    <div> <span className={styles.shimmer}>Favorite Asks</span></div>
-                </div>
-                <div>
-                    {favoriteAsks.map((ask) => (
-                        <div>
-                            <p>{ask.title}</p>
-                            <p>{ask.description}</p>
-                            <p>{ask.user.displayName}</p>
-                        </div>
-                    ))}
-                </div>
+            <div style={{ display: 'flex', gap: '10px', fontSize: '1.75rem', fontWeight: '550', marginBottom: '10px' }}>
+                <div> <span className={styles.shimmer}>Favorite Asks</span></div>
             </div>
+            {favoriteAsks.map((ask) => (
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    padding: '10px 35px',
+                    gap: '18px',
+                    border: '1px solid #fff9e6',
+                    marginBottom: '10px',
+                    borderRadius: '5px',
+                    color: '#fff9e6',
+                    position: 'relative',
+                    background: 'linear-gradient(to right, rgba(84, 0, 55, 0.2), rgba(199, 21, 133, 0.2))',
+                    backdropFilter: 'blur(10px)',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.08)',
+                    transition: 'all 0.3s ease',
+                    fontSize: '1.1rem',
+                }}>
+                    <div style={{ display: 'flex', gap: '10px', flex: 1, justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+
+                            <div>
+                                <p className={styles.titleText}>{ask.title}</p>
+                                <p className={styles.descriptionText}>{ask.description}</p>
+                                <p><a href={`user/${ask.user.id}`} className={styles.userLink}>{ask.user.displayName}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </>
     )
 }
--- frontend/src/components/UserInfo/FavoriteOffersFeed.tsx
+++ frontend/src/components/UserInfo/FavoriteOffersFeed.tsx
@@ -14,20 +14,38 @@ function FavoriteOffersFeed() {
     }
     return (
         <>
-            <div style={{ flex: 1 }}>
-                <div style={{ display: 'flex', gap: '10px', fontSize: '1.75rem', fontWeight: '550', marginBottom: '10px' }}>
-                    <div> <span className={styles.shimmer}>Favorite Offers</span></div>
-                </div>
-                <div>
-                    {favoriteOffers.map((offer) => (
-                        <div>
-                            <p>{offer.title}</p>
-                            <p>{offer.description}</p>
-                            <p>{offer.user.displayName}</p>
-                        </div>
-                    ))}
-                </div>
+            <div style={{ display: 'flex', gap: '10px', fontSize: '1.75rem', fontWeight: '550', marginBottom: '10px' }}>
+                <div> <span className={styles.shimmer}>Favorite Offers</span></div>
             </div>
+
+            {favoriteOffers.map((offer) => (
+                <div key={offer.id} style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    padding: '10px 35px',
+                    gap: '18px',
+                    border: '1px solid #fff9e6',
+                    marginBottom: '10px',
+                    borderRadius: '5px',
+                    color: '#fff9e6',
+                    position: 'relative',
+                    background: 'linear-gradient(to right, rgba(84, 0, 55, 0.2), rgba(199, 21, 133, 0.2))',
+                    backdropFilter: 'blur(10px)',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.08)',
+                    transition: 'all 0.3s ease',
+                    fontSize: '1.1rem',
+                }}>
+                    <div style={{ display: 'flex', gap: '10px', flex: 1, justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                            <p className={styles.titleText}>{offer.title}</p>
+                            <p className={styles.descriptionText}>{offer.description}</p>
+                            <p><a href={`/user/${offer.user.id}`} className={styles.userLink}>{offer.user.displayName}</a></p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
         </>
     )
 }
